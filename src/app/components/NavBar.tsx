f36398517ea461d10addd57d73360ec6f10d75e1
MatheusIzaquiel <matheusizaquiel1898@gmail.com>
2025-09-11 19:42:24 -0300

    Adicionando Navbar

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="fixed top-0 w-full flex items-center justify-between py-2 px-8 z-50 bg-slate-800 text-gray-300">
+          <Link 
+            href="/"
+            className="uppercase font-bold text-md h-12 flex items-center"
+          >
+            next storage
+          </Link>
+        </nav>
+  )
+}
